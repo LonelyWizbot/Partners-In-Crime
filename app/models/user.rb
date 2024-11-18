73df9c6b0824b0997ec827b0b11d8db38f123836
parwez amini <cmoiadib@gmail.com>
2024-11-18 16:51:54 +0100

    Added models validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ApplicationRecord
   has_many :partners
+  has_many :bookings
+  has_many :partners, through: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :address, presence: true
+  validates :zip_code, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :phone_number, numericality: true, length: { minimum: 10, maximum: 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_145051) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_150842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.text "message"
+    t.float "total_price"
+    t.integer "status", default: 0
+    t.bigint "user_id", null: false
+    t.bigint "partner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["partner_id"], name: "index_bookings_on_partner_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "partners", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "image"
-    t.integer "status"
+    t.boolean "status", default: true
     t.float "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -34,9 +48,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_145051) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "additional_address"
+    t.integer "zip_code"
+    t.string "city"
+    t.string "country"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "partners"
+  add_foreign_key "bookings", "users"
   add_foreign_key "partners", "users"
 end
