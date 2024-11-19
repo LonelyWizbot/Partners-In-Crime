cf0bfa1b651181bcdc6a961b221f558d5528a892
Emilie Didier <emilie.didier97@gmail.com>
2024-11-19 16:09:15 +0100

    modified show page, added carousel, and style

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_150842) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
+    t.string "first_name" 
     t.string "last_name"
     t.string "address"
     t.string "additional_address"
